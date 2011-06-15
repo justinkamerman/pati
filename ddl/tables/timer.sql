7c30dd43fb788f989437fcd966bbb84e978c3ee1
Damien Dubois <ddubois@unb.ca>
2011-06-15 21:41:37 +0000

    Table structure for timer

---
+++ ddl/tables/timer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE  `Indexerdb`.`timer` (
+  `ID` int(11) NOT NULL DEFAULT '1',
+  `LatestTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1
+
+Insert into Indexerdb.timer (Id, LatestTime) values (1,0),(2,0) ;
+
+
