317a96e6cf55815cdcec4f413eb3a5a4b1f5dd06
Justin Kamerman <junk@kaleco.net>
2012-10-16 11:08:23 -0300

    Clean up for opensourcing

--- bin/indexer.py
+++ bin/indexer.py
@@ -1,10 +1,10 @@
 #!usr/bin/python
-
-# 	Used to get the system time and to send commands to the terminal which launched 
+#
+# Used to get the system time and to send commands to the terminal which launched 
 # this program.
 import time,os
 
-#	Used to clear the system screen.
+# Used to clear the system screen.
 os.system( 'clear' )
 
 print '**********************************'
@@ -12,24 +12,21 @@ print '*    STARTING INDEXING CYCLE     *'
 print '**********************************\n'
 
 
-# 	List of command to execute often
-command0 = '/home/damien/indexer/Indexer.sh'
+#       List of command to execute often
+command0 = 'bin/indexer.sh'
 
-
-
-
-#	System Times
-call = os.popen( "mysql --host=131.202.242.164  --user=indexer --password=indexer2011 indexerdb --execute=\"select count(*) from timer where ID=2 AND LatestTime=0\" --skip-column-names" )	
+#  System Times
+call = os.popen( "mysql --host=localhost  --user=xxx --password=xxx indexerdb --execute=\"select count(*) from timer where ID=2 AND LatestTime=0\" --skip-column-names" )       
 time = call.readline()
 call.close()
 time = int( time )
-os.chdir( '/home/damien/indexer/' )
+os.chdir( 'workspace/' )
 
 
 while( time==1 ):
-	os.system( command0 )
-	call = os.popen( "mysql --host=131.202.242.164  --user=indexer --password=indexer2011 indexerdb --execute=\"select count(*) from timer where ID=2 AND LatestTime=0\" --skip-column-names" )	
-	time = call.readline()
-	call.close()
-	time = int( time )
+        os.system( command0 )
+        call = os.popen( "mysql --host=localhost  --user=xxx --password=xxx indexerdb --execute=\"select count(*) from timer where ID=2 AND LatestTime=0\" --skip-column-names" )       
+        time = call.readline()
+        call.close()
+        time = int( time )
 
--- bin/process_controller.py
+++ bin/process_controller.py
@@ -1,10 +1,11 @@
 #!usr/bin/python
-
-# 	Used to get the system time and to send commands to the terminal which launched 
+#
+# Used to get the system time and to send commands to the terminal which launched 
 # this program.
+#
 import time,os
 
-#	Used to clear the system screen.
+# Used to clear the system screen.
 os.system( 'clear' )
 
 
@@ -16,24 +17,21 @@ print '*    STARTING INDEXING CYCLE     *'
 print '**********************************\n'
 
 
-# 	List of command to execute often
-command0 = '/home/raman/WorkspaceIndexer/fcs6999/Indexer.sh'
+# List of command to execute often
+command0 = 'bin/Indexer.sh'
 
-
-
-
-#	System Times
+# System Times
 start = time.time()
 
 current = time.time()
 loop=0;
-os.chdir( '/home/raman/WorkspaceIndexer/fcs6999/' )
+os.chdir( 'workspace/' )
 while( loop < numberOfLoop ):
-	
+        
 
-	os.system( command0 )
-	current = time.time()
-	elapsed = current - start
-	print ' \n time for loop ' + str(loop) + ' is ' + str(elapsed) + '\n'
-	loop = loop + 1
+        os.system( command0 )
+        current = time.time()
+        elapsed = current - start
+        print ' \n time for loop ' + str(loop) + ' is ' + str(elapsed) + '\n'
+        loop = loop + 1
 
